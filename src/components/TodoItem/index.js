6b5f30b757ae75be8385e6c23d635af00b7a483b
rakesh <modir810@gmail.com>
2023-03-07 08:36:26 +0000

    Simple Todos

---
+++ src/components/TodoItem/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+
+const TodoItem = props => {
+  const {title, onDelete, uniqueId} = props
+  const performDelete = () => {
+    onDelete(uniqueId)
+  }
+  return (
+    <li className="item-container">
+      <p className="para">{title}</p>
+      <button type="button" className="btn" onClick={performDelete}>
+        Delete
+      </button>
+    </li>
+  )
+}
+export default TodoItem
